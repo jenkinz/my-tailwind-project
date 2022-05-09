5be2c0fb2227439882295360d159bc061149e64e
Brian Jenkins <bj@jenkinz.com>
2022-05-09 11:30:05 -0700

    Strip unused css with purgecss for production

--- postcss.config.js
+++ postcss.config.js
@@ -3,5 +3,12 @@ module.exports = {
     plugins: [
         require('tailwindcss'),
         require('autoprefixer'),
+        // strip unused css when compiling for production:
+        process.env.NODE_ENV === 'production' && require('@fullhuman/postcss-purgecss')({
+            content: [
+                './public/**/*.{html,js}'
+            ],
+            defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+        })
     ]
 }
